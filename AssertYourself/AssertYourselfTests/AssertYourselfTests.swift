708e773852f235c73d646dd83e614d2910746eae
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-05 11:56:21 +0700

    avoid condition in Tests

--- AssertYourself/AssertYourselfTests/AssertYourselfTests.swift
+++ AssertYourself/AssertYourselfTests/AssertYourselfTests.swift
@@ -10,10 +10,29 @@ import XCTest
 
 class AssertYourselfTests: XCTestCase {
     func test_fail(){
-//        XCTFail()
+        XCTFail()
     }
     
     func test_fail_withSimpleMessage(){
         XCTFail("we have a problem")
+    }
+    
+    func test_fail_withInterpolatedMessage(){
+        let theAnswer = 40
+        XCTFail("the answer is \(theAnswer)")
+    }
+    
+    //MARK: Don't do this!
+    func test_fail_avoidConditionalCode(){
+        let success = true
+        if !success {
+            XCTFail()
+        }
+    }
+    
+    //MARK: Do this!
+    func test_fail_asserttrue(){
+        let success = true
+        XCTAssertTrue(success)
     }
 }
