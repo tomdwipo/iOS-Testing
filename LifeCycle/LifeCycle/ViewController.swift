77ba35636ae98a794f7fe8506e3f5fbf9db30ae9
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-06 19:04:54 +0700

    add project to test lifecycle

---
+++ LifeCycle/LifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by RATAMATE on 06/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
