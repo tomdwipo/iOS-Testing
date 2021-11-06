ace2ef6b38e15a61688a0dcf135f0d84c24bfc2d
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-06 19:15:57 +0700

    start test zero to indicate it precedes the first real test we want to write

---
+++ LifeCycle/LifeCycle/MyClass.swift
@@ -0,0 +1,18 @@
+//
+//  MyClass.swift
+//  LifeCycle
+//
+//  Created by RATAMATE on 06/11/21.
+//
+
+import Foundation
+
+class MyClass {
+    func methodOne(){
+        print(">> methodOne")
+    }
+    
+    func methodTwo(){
+        print(">> methodTwo")
+    }
+}
---
+++ LifeCycle/LifeCycleTests/MyClassTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyClassTests.swift
+//  LifeCycleTests
+//
+//  Created by RATAMATE on 06/11/21.
+//
+
+import XCTest
+
+class MyClassTests: XCTestCase {
+
+    func test_zero(){
+        XCTFail("Tests not yet implemented in MyClassTests")
+    }
+
+}
