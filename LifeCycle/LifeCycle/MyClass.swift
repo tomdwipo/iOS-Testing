ebcf96bf4c0a73686e8018635e838afe4659cb26
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-06 19:32:14 +0700

    test calls the four phase test

--- LifeCycle/LifeCycle/MyClass.swift
+++ LifeCycle/LifeCycle/MyClass.swift
@@ -8,6 +8,19 @@
 import Foundation
 
 class MyClass {
+    private static var allInstances = 0
+    private let instance: Int
+    
+    init(){
+        MyClass.allInstances += 1
+        instance = MyClass.allInstances
+        print(">> MyClass.init() #\(instance)")
+    }
+    
+    deinit {
+        print(">> MyClass.deinit() #\(instance)")
+    }
+    
     func methodOne(){
         print(">> methodOne")
     }
--- LifeCycle/LifeCycleTests/MyClassTests.swift
+++ LifeCycle/LifeCycleTests/MyClassTests.swift
@@ -6,11 +6,28 @@
 //
 
 import XCTest
+@testable import LifeCycle
 
 class MyClassTests: XCTestCase {
 
-    func test_zero(){
-        XCTFail("Tests not yet implemented in MyClassTests")
+    //MARK: after infra is okay delete them
+//    func test_zero(){
+//        XCTFail("Tests not yet implemented in MyClassTests")
+//    }
+    
+    func test_methodOne(){
+        let sut = MyClass()
+        
+        sut.methodOne()
+        
+        XCTFail("Failed, ya")
+    }
+    
+    func test_methodTwo(){
+        let sut = MyClass()
+        
+        sut.methodTwo()
+        
     }
 
 }
