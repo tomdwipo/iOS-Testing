472b6d25820adb349c524780415e66c59a0ec471
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-06 20:10:07 +0700

    add setup and teardown to protect clean room for any tests we write

--- LifeCycle/LifeCycleTests/MyClassTests.swift
+++ LifeCycle/LifeCycleTests/MyClassTests.swift
@@ -9,15 +9,24 @@ import XCTest
 @testable import LifeCycle
 
 class MyClassTests: XCTestCase {
-    let sut = MyClass()
+    private var sut: MyClass!
 
     //MARK: after infra is okay delete them
 //    func test_zero(){
 //        XCTFail("Tests not yet implemented in MyClassTests")
 //    }
     
+    override func setUp() {
+        super.setUp()
+        sut = MyClass()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
     func test_methodOne(){
-        
         sut.methodOne()
         
 //        XCTFail("Failed, ya")
