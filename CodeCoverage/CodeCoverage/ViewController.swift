e956b15bb1f9d4871f1004ded79e1b474a2eeedf
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-07 15:30:12 +0700

    add CodeCoverage project to test code coverage

---
+++ CodeCoverage/CodeCoverage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeCoverage
+//
+//  Created by RATAMATE on 07/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
