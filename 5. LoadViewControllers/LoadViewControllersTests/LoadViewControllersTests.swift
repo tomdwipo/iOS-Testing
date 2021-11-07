311e62371f34d6663ea97cc947918dcb04f55a3d
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-07 21:28:53 +0700

    add new project LoadViewControllers

---
+++ 5. LoadViewControllers/LoadViewControllersTests/LoadViewControllersTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoadViewControllersTests.swift
+//  LoadViewControllersTests
+//
+//  Created by RATAMATE on 07/11/21.
+//
+
+import XCTest
+@testable import LoadViewControllers
+
+class LoadViewControllersTests: XCTestCase {
+
+    func test_zero(){
+        XCTFail("Tests not yet implemented in LoadViewControllers")
+    }
+
+}
