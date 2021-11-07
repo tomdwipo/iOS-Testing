ca166fe613623aa433d775b721fef84dd8cc9c5a
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-07 21:39:37 +0700

    take control of application launch

---
+++ 5. LoadViewControllers/LoadViewControllers/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  LoadViewControllers
+//
+//  Created by RATAMATE on 07/11/21.
+//
+
+import Foundation
+import UIKit
+
+let appDelegate: AnyClass = NSClassFromString("TestingAppDelegate") ?? AppDelegate.self
+
+UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(appDelegate))
---
+++ 5. LoadViewControllers/LoadViewControllersTests/TestingAppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  TestingAppDelegate.swift
+//  LoadViewControllersTests
+//
+//  Created by RATAMATE on 07/11/21.
+//
+
+import UIKit
+
+@objc(TestingAppDelegate)
+class TestingAppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("from TestingAppDelegate")
+        return true
+    }
+
+
+
+}
+
