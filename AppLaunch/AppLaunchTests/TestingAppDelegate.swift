30ed2a512b2d4c3c8cb04770751dbd2d34b0b113
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-07 17:32:43 +0700

    updated

--- AppLaunch/AppLaunchTests/TestingAppDelegate.swift
+++ AppLaunch/AppLaunchTests/TestingAppDelegate.swift
@@ -11,8 +11,6 @@ import UIKit
 @objc(TestingAppDelegate)
 class TestingAppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print("<< Launching with testing app delegate")
         return true
