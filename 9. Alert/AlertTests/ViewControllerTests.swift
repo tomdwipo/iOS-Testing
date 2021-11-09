648d3280027d00ef9ae50ced1222bb800438e24b
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-09 15:00:03 +0700

    add alert test

--- 9. Alert/AlertTests/ViewControllerTests.swift
+++ 9. Alert/AlertTests/ViewControllerTests.swift
@@ -11,32 +11,37 @@ import ViewControllerPresentationSpy
 
 class ViewControllerTests: XCTestCase {
     private var alertVerifier: AlertVerifier!
+    private var sut: ViewController!
     
     override func setUp() {
         super.setUp()
+
         alertVerifier = AlertVerifier()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: String(describing: ViewController.self)) as? ViewController
+        sut.loadViewIfNeeded()
     }
     
     override func tearDown() {
         alertVerifier = nil
+        sut = nil
         super.tearDown()
     }
     
     func test_outlets_shouldBeConnected(){
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let sut = storyboard.instantiateViewController(withIdentifier: String(describing: ViewController.self)) as! ViewController
-        sut.loadViewIfNeeded()
-        
         XCTAssertNotNil(sut.button, "button")
     }
     
     func test_tapingButton_shouldShowAlert(){
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let sut = storyboard.instantiateViewController(withIdentifier: String(describing: ViewController.self)) as! ViewController
-        sut.loadViewIfNeeded()
+      
         tap(sut.button)
         alertVerifier.verify(title: "Do the thing?", message: "Let us know if you want to do thing", animated: true, actions: [.cancel("Cancel"), .default("OK")], presentingViewController: sut)
 
         XCTAssertEqual(alertVerifier.preferredAction?.title, "OK", "prefered action")
+    }
+    
+    func test_executeAlertAction_withOkButton() throws {
+        tap(sut.button)
+        try alertVerifier.executeAction(forButton: "OK")
     }
 }
