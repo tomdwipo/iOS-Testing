7e438fef902336600323a4df528bb64ceec5187d
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-14 12:23:54 +0700

    Textfield project starter

---
+++ 14. TextField/TextField/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  TextField
+//
+//  Created by RATAMATE on 14/11/21.
+//
+
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("Launch from AppDelegate")
+        return true
+    }
+
+}
+
---
+++ 14. TextField/TextFieldTests/TestingAppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  TestingAppDelegate.swift
+//  TextFieldTests
+//
+//  Created by RATAMATE on 14/11/21.
+//
+
+import UIKit
+
+@objc(TestingAppDelegate)
+class TestingAppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("Launch from TestingAppDelegate")
+        return true
+    }
+
+}
+
