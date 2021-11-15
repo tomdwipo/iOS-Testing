872e919933054a2ee942f1ed94cfe7dbcf03bba6
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-15 20:33:06 +0700

    refactoring project starter

---
+++ 17. Refactoring/Refactoring/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Refactoring
+//
+//  Created by RATAMATE on 15/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
