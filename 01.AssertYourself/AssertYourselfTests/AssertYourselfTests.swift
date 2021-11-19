6c6fda3def63735e7f3b99f7c75a7e01cd63ce4e
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-19 11:23:30 +0700

    update tests

--- 01.AssertYourself/AssertYourselfTests/AssertYourselfTests.swift
+++ 01.AssertYourself/AssertYourselfTests/AssertYourselfTests.swift
@@ -9,18 +9,18 @@ import XCTest
 @testable import AssertYourself
 
 class AssertYourselfTests: XCTestCase {
-    func test_fail(){
-        XCTFail()
-    }
+//    func test_fail(){
+//        XCTFail()
+//    }
     
-    func test_fail_withSimpleMessage(){
-        XCTFail("we have a problem")
-    }
+//    func test_fail_withSimpleMessage(){
+//        XCTFail("we have a problem")
+//    }
     
-    func test_fail_withInterpolatedMessage(){
-        let theAnswer = 40
-        XCTFail("the answer is \(theAnswer)")
-    }
+//    func test_fail_withInterpolatedMessage(){
+//        let theAnswer = 40
+//        XCTFail("the answer is \(theAnswer)")
+//    }
     
     //MARK: Don't do this!
     func test_fail_avoidConditionalCode(){
@@ -39,46 +39,46 @@ class AssertYourselfTests: XCTestCase {
         }
        
     }
-    
-    func test_assertNil(){
-        let optionalValue: Int? = 20
-        XCTAssertNil(optionalValue)
-    }
-    
-    func test_assertNil_withSimpleStruct(){
-        let optionalValue: SimpleStruct? = SimpleStruct(x: 1, y: 2)
-        XCTAssertNil(optionalValue)
-    }
-    
-    func test_assertNil_withSelfDescribingType(){
-        let optionalValue: StructWithDescribing? = StructWithDescribing(x: 1, y: 2)
-        XCTAssertNil(optionalValue)
-    }
-    
-    func test_assertEqual(){
-        let actual = "actual"
-        XCTAssertEqual(actual, "expected")
-    }
-    
-    func test_assertEqual_withOptional(){
-        let result: String? = "foo"
-        XCTAssertEqual(result, "bar")
-    }
-    
-    func test_floatingPointDanger(){
-        let result = 0.1 + 0.2
-        XCTAssertEqual(result, 0.3)
-    }
+//
+//    func test_assertNil(){
+//        let optionalValue: Int? = 20
+//        XCTAssertNil(optionalValue)
+//    }
+//
+//    func test_assertNil_withSimpleStruct(){
+//        let optionalValue: SimpleStruct? = SimpleStruct(x: 1, y: 2)
+//        XCTAssertNil(optionalValue)
+//    }
+//
+//    func test_assertNil_withSelfDescribingType(){
+//        let optionalValue: StructWithDescribing? = StructWithDescribing(x: 1, y: 2)
+//        XCTAssertNil(optionalValue)
+//    }
+//
+//    func test_assertEqual(){
+//        let actual = "actual"
+//        XCTAssertEqual(actual, "expected")
+//    }
+//
+//    func test_assertEqual_withOptional(){
+//        let result: String? = "foo"
+//        XCTAssertEqual(result, "bar")
+//    }
+//
+//    func test_floatingPointDanger(){
+//        let result = 0.1 + 0.2
+//        XCTAssertEqual(result, 0.3)
+//    }
     
     func test_floatingPointFixed(){
         let result = 0.1 + 0.2
         XCTAssertEqual(result, 0.3, accuracy: 0.0001)
     }
     
-    func test_messageOverKill(){
-        let actual = "Actual"
-        XCTAssertEqual(actual, "expected", "expected \"expected\" but got \"\(actual)\"")
-    }
+//    func test_messageOverKill(){
+//        let actual = "Actual"
+//        XCTAssertEqual(actual, "expected", "expected \"expected\" but got \"\(actual)\"")
+//    }
 }
 
 struct SimpleStruct {
